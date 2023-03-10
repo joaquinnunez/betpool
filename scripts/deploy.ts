cfb9e678dfa38058f91cd123592063bc7aa6b0f6
Joaquín Núñez <joaquin.nunez@protonmail.com>
2023-03-10 09:47:28 -0300

    fix: Remove `console.log`

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -5,12 +5,12 @@ async function main() {
   const outcomes = signers.slice(1, 5)
   const PoolConfContract = await ethers.getContractFactory('PoolConfiguration')
   const poolConf = await PoolConfContract.deploy(100, ethers.utils.parseEther('1.0'))
+  await poolConf.deployed();
   const Pool = await ethers.getContractFactory('Pool');
   const pool = await Pool.deploy(outcomes.map(o=>o.address), poolConf.address);
 
   await pool.deployed();
   console.log(pool.address)
-  console.log('Done.');
 }
 
 // We recommend this pattern to be able to use async/await everywhere
